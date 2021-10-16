73657b61cd619e27ba90fbcf7b820161d81641b2
Vitamin <vitamin937@rambler.ru>
2021-10-16 14:01:22 +0300

    new commit

---
+++ geek1/src/ru/geek1/lesson5/Cat.java
@@ -0,0 +1,50 @@
+package ru.geek1.lesson5;
+
+public class Cat {
+    private String name;
+    private String color;
+    private int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public Cat(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age >= 0) {
+            this.age = age;
+        }else {
+            System.out.println("Возрвст не может быть меньше нуля");
+        }
+    }
+
+    void info(){
+        System.out.println(name + " " +color + " " + age);
+    }
+}
---
+++ geek1/src/ru/geek1/lesson5/MainCat.java
@@ -0,0 +1,21 @@
+package ru.geek1.lesson5;
+
+import java.util.Arrays;
+
+public class MainCat {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("qwer","qwer",12);
+        Cat cat2 = new Cat("qweq","asdf");
+
+        cat1.setName("Vaska");
+        cat1.setColor("Black");
+        cat1.setAge(5);
+
+        cat2.setName("Barsik");
+        cat2.setColor("Red");
+        cat2.setAge(-10);
+
+        cat1.info();
+        cat2.info();
+    }
+}
