9d1918836e7fbe14f8cc6f0f166d87e7f53821c8
Vitamin <vitamin937@rambler.ru>
2021-10-19 18:21:07 +0300

    18/10/2021 9

--- geek1/src/ru/geek1/lesson7/WWindows.java
+++ geek1/src/ru/geek1/lesson7/WWindows.java
@@ -50,56 +50,6 @@ public class WWindows extends JFrame {
     }
 }
 
-
-//public class NextWindow extends JFrame{
-//    public NextWindow() {
-//        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-//        setTitle("Next Window");
-//        setBounds(300, 300, 400, 400);
-//        setVisible(true);
-//
-//        JButton jButtonOK = new JButton("OK");
-//        JButton jButtonExt = new JButton("Cancel");
-//        JTextField tfName = new JTextField("");
-//        JTextField tfsName = new JTextField("");
-//
-//        JPanel panel1 = new JPanel(new GridLayout(1, 2));
-//        panel1.add(jButtonOK);
-//        panel1.add(jButtonExt);
-//
-//        JPanel panel2 = new JPanel(new GridLayout(2, 1));
-//        panel2.add(tfName);
-//        panel2.add(tfsName);
-//
-//        panel1.setPreferredSize(new Dimension(100, 70));
-//        panel2.setPreferredSize(new Dimension(100, 70));
-//
-//        JPanel panel3 = new JPanel(new GridLayout(2, 1));
-//        panel3.add(panel2);
-//        panel3.add(panel1);
-//
-//        add(panel3);
-//
-//
-//        jButtonOK.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//                System.out.println(tfName.getText());
-//                System.out.println(tfsName.getText());
-//
-//            }
-//        });
-//        jButtonExt.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                dispose();
-//            }
-//        });
-//    }
-//}
-
-
 class MainWClass {
     public static void main(String[] args) {
         new WWindows();
